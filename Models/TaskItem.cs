e70cd6a712b5995e2608b19ef642b84265fa2b10
Sam Forrest <samueljamesforrestweb@gmail.com>
2024-07-17 09:33:56 +0100

    IMP: Made task descriptions nullable and added yellow variant of button for edit buttons

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -7,7 +7,7 @@ namespace DotnetPlayground.Models
         public int Id { get; set; }
 
         [Required]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         public bool IsComplete { get; set; }
     }
